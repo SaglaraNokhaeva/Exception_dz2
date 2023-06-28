f7d00ea59689b74270e8a07d99497c9e96b392d5
Saglara <nohaevass@yandex.ru>
2023-06-28 16:47:38 +0300

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,6 +1,6 @@
 public class ex2 {
     public static void main(String[] args) {
-        double [] intArray = new double[]{0,1,2,3,4,5,6,7,8};
+        int [] intArray = new int []{0,1,2,3,4,5,6,7,8};
         try {
             int d = 0;
             double catchedRes1 = intArray[8] / d;
@@ -8,6 +8,5 @@ public class ex2 {
         } catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
         }
-
     }
 }
