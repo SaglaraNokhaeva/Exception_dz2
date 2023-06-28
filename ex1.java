e89ea3b36f008d8765b94d4ff48fa6b5993e5d44
Saglara <nohaevass@yandex.ru>
2023-06-28 15:59:54 +0300

    Create ex1.java

---
+++ ex1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), и
+возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения,
+вместо этого, необходимо повторно запросить у пользователя ввод данных.
+ */
+public class ex1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите дробное число: ");
+        float num = 0;
+        try {
+            num = scan.nextFloat();
+            System.out.printf("Ваше число: %f \n", num);
+            scan.close();
+        } catch (Exception e) {
+            System.out.println("Введите дробное число: ");
+        }
+    }
+}
